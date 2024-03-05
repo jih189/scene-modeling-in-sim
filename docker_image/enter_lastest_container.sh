2153d86531e05f0c8dbe8fdd544743ff736bbdcb
sudhansh <sudhansh6@gmail.com>
2024-03-05 15:22:44 -0800

    initial commit

--- docker_image/enter_lastest_container.sh
+++ docker_image/enter_lastest_container.sh
@@ -2,7 +2,7 @@
 container_count=$(docker ps | wc -l | awk '{print $1-1}')
 if [ $container_count -eq 1 ]; then
 	last_container_id=$(docker ps --format "{{.ID}}" | head -n 1)
-	docker exec -it $last_container_id /bin/bash
+	docker exec -e DISPLAY=$DISPLAY -it $last_container_id /bin/bash
 else
 	echo "you have $container_count container, you should have only one!!"
 fi
