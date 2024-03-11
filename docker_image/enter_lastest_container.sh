130c483f3c347094decbdd5b632149eda43103b3
jih189 <jih189@ucsd.edu>
2024-03-11 12:11:46 -0700

    update enter_lastest_container.sh

--- docker_image/enter_lastest_container.sh
+++ docker_image/enter_lastest_container.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-container_count=$(docker ps | wc -l | awk '{print $1-1}')
+IMAGE_NAME="scene_modeling"
+container_count=$(docker ps | grep $IMAGE_NAME | wc -l)
 if [ $container_count -eq 1 ]; then
-	last_container_id=$(docker ps --format "{{.ID}}" | head -n 1)
-	docker exec -e DISPLAY=$DISPLAY -it $last_container_id /bin/bash
+	last_container_id=$(docker container ls  --filter=ancestor=$IMAGE_NAME --format "{{.ID}}")
+	docker exec -it $last_container_id /bin/bash
 else
-	echo "you have $container_count container, you should have only one!!"
+	echo "you have $container_count container with image $IMAGE_NAME, you should have only one!!"
 fi
